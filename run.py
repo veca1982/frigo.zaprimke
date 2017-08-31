634b517ac3e254bbc667e002be6f54088b43ca88
veca1982 <vedran.krtalic@yahoo.com>
2017-08-31 20:10:15 +0200

    make it better

--- run.py
+++ run.py
@@ -6,8 +6,12 @@ import sys
 from app import create_app
 
 
-config_name = os.getenv('FLASK_CONFIG')
-#config_name = 'development'
+#config_name = os.getenv('FLASK_CONFIG')
+config_name = 'production'
 app = create_app(config_name)
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
+
+if __name__ == '__main__':
+    app.run()
