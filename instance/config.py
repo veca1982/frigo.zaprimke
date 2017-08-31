ca70ad7d475d36630642c072a9198ec54bc6419d
veca1982 <vedran.krtalic@yahoo.com>
2017-08-31 19:47:10 +0200

    make it better

--- instance/config.py
+++ instance/config.py
@@ -1,4 +1,4 @@
 # instance/config.py
 
-SECRET_KEY = 'p9Bv<3Eid9%$i01'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:ivavedran@localhost:5432/utilitymeter'
+SECRET_KEY = '7228e25f3eaeb82bc02be55ba3c3f1fb6f96a1a9383c0722be65da0965c58afd'
+SQLALCHEMY_DATABASE_URI = 'postgres://vetskieamburwa:7228e25f3eaeb82bc02be55ba3c3f1fb6f96a1a9383c0722be65da0965c58afd@ec2-184-73-189-221.compute-1.amazonaws.com:5432/d589d4h3qqiui'
