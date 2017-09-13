6a04d3e8faf9e053544ba2cdfbcbfc2054408566
veca1982 <vedran.krtalic@yahoo.com>
2017-09-13 15:52:54 +0200

    tekst commita

--- run.py
+++ run.py
@@ -9,8 +9,8 @@ from app import create_app
 config_name = os.getenv('FLASK_CONFIG')
 #config_name = 'production'
 app = create_app(config_name)
-#reload(sys)
-#sys.setdefaultencoding('utf-8')
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 
 if __name__ == '__main__':
