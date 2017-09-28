bca0fdda3f07a4913f07791af77b6810c39d9cfd
veca1982 <vedran.krtalic@yahoo.com>
2017-09-28 22:08:14 +0200

    Dodan novi paginator!

--- app/home/views.py
+++ app/home/views.py
@@ -58,9 +58,9 @@ def zaprimke():
     num_items_per_page = 10
     __check_user_has_role()
     num_of_items = Zaprimka.query.count()
-    #pages = make_paginator(range(1, int(np.ceil(num_of_items/10.00))+1), active_page=1)
+    pages = make_paginator(range(1, int(np.ceil(num_of_items/10.00))+1), active_page=1)
     #paginator = Paginator(num_items=num_of_items, num_items_per_page=num_items_per_page, max_pages_per_block=5)
-    pages = paginator.make_paginator(active_page=1)
+    #pages = paginator.make_paginator(active_page=1)
     #zaprimkas = Zaprimka.query.limit(10).all()
     zaprimkas = Zaprimka.query.order_by(Zaprimka.id.desc()).paginate(1, num_items_per_page, error_out=False).items
 
