94a7a4f69fe24776a98ad7cd1e9b2a2e63f20536
veca1982 <vedran.krtalic@yahoo.com>
2018-09-29 13:20:32 +0200

    prepravka uyimanja cijene za zaprimku upita

--- app/admin/views.py
+++ app/admin/views.py
@@ -370,8 +370,9 @@ def add_cijena():
             last_cijena = get_last_active_cijena(caliber)
             try:
                 # add nova cijena
-                last_cijena.datum_do = cijena.datum_od
-                db.session.add(last_cijena)
+                if last_cijena != None:
+                    last_cijena.datum_do = cijena.datum_od.timedelta(days=-1)
+                    db.session.add(last_cijena)
                 db.session.add(cijena)
                 db.session.commit()
                 flash('Uspješno ste dodali novu cijenu.')
